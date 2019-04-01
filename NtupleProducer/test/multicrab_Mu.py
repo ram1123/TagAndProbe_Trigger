fd2640ca3c000c2c5d1494fb8e2f1e442eea6629
arun <arun.kumar@cern.ch>
2019-04-01 11:27:24 +0200

    up

--- NtupleProducer/test/multicrab_Mu.py
+++ NtupleProducer/test/multicrab_Mu.py
@@ -1,30 +1,26 @@
-
-name = 'MuonNtuple_HWW_2017_fixed_v2_92X'
+#name = 'TrigEff_HWW_EGamma_2018'
 
 dataset = {
-   'Run2017B' : '/SingleMuon/Run2017B-31Mar2018-v1/MINIAOD',
-   'Run2017C' : '/SingleMuon/Run2017C-31Mar2018-v1/MINIAOD', 
-   'Run2017D' : '/SingleMuon/Run2017D-31Mar2018-v1/MINIAOD', 
-   'Run2017E' : '/SingleMuon/Run2017E-31Mar2018-v1/MINIAOD', 
-   'Run2017F' : '/SingleMuon/Run2017F-31Mar2018-v1/MINIAOD', 
+   'Run2018A' : '/SingleMuon/Run2018A-17Sep2018-v2/MINIAOD',
+   'Run2018B' : '/SingleMuon/Run2018B-17Sep2018-v1/MINIAOD', 
+   'Run2018C' : '/SingleMuon/Run2018C-17Sep2018-v1/MINIAOD', 
+   'Run2018D' : '/SingleMuon/Run2018D-PromptReco-v2/MINIAOD', 
    }
 
 
 #nevents = -1 
 lumisPerJob = {
-   'Run2017B':        100,
-   'Run2017C':        100,
-   'Run2017D':        100,
-   'Run2017E':        100,
-   'Run2017F':        100,
+   'Run2018A':        100,
+   'Run2018B':        100,
+   'Run2018C':        100,
+   'Run2018D':        100,
    }
 
 listOfSamples = [
-   'Run2017B',        
-   'Run2017C',        
-   'Run2017D',        
-   'Run2017E',        
-   'Run2017F',        
+   'Run2018A',        
+   'Run2018B',        
+   'Run2018C',        
+   'Run2018D',        
    ]
 
 if __name__ == '__main__':
@@ -38,7 +34,7 @@ if __name__ == '__main__':
    def submit(config):
        res = crabCommand('submit', config = config)
 
-   config.General.workArea = 'crab_'+name
+   config.General.workArea = 'crab_TrigEff_HWW_SingleMu12_23'
    config.General.transferLogs = False
 
    config.JobType.pluginName = 'Analysis'
@@ -47,12 +43,13 @@ if __name__ == '__main__':
 
    config.Data.inputDBS = 'global'
    config.Data.splitting = 'LumiBased'
-   config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'
+   config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PromptReco/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt'
+  # config.Data.lumiMask = '/afs/cern.ch/work/a/arun/Latinos/RunIILegacy/TriggerEff/CMSSW_10_2_11/src/TagAndProbe_Trigger/NtupleProducer/test/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt'
    config.Data.publication = False
    config.Data.totalUnits = -1
-   config.Data.outLFNDirBase = '/store/user/skeshri/' + name
+   config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshww/arun/TriggerEff_RunIILegacy/TrigEff_HWW_SingleMu12_23'
 
-   config.Site.storageSite = 'T2_IN_TIFR'
+   config.Site.storageSite ='T2_CH_CERN'
  #  config.Site.blacklist = ['T2_BR_SPRACE', 'T2_US_Wisconsin', 'T1_RU_JINR', 'T2_RU_JINR', 'T2_EE_Estonia']
 
    listOfSamples.reverse()
